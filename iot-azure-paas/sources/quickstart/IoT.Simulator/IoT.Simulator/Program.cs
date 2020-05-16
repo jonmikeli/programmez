82e9f75b45beb68247c20b9ab2f8f6b4b65f3bf1
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-05-16 21:06:19 +0200

    updates

--- iot-azure-paas/sources/quickstart/IoT.Simulator/IoT.Simulator/Program.cs
+++ iot-azure-paas/sources/quickstart/IoT.Simulator/IoT.Simulator/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Devices.Client;
 
 namespace IoT.Simulator
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Programmez!-Quickstart-Simulateur ReButton");
+
+            try
+            {
+                using (DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("TO BE REPLACED"))
+                {
+
+                    //schema according to official documentation
+                    var reButtonAction = new
+                    {
+                        actionNum = "1",
+                        message = "Single click",
+                        singeClick = "Single click",
+                        batteryVoltage = 2.8
+                    };
+
+                    string jsonData = JsonSerializer.Serialize(reButtonAction);
+
+                    var message = new Message(Encoding.UTF8.GetBytes(jsonData));
+
+                    //metadata to define the message type
+                    message.Properties.Add("messageType", "data");
+
+                    message.ContentType = "application/json";
+                    message.ContentEncoding = "utf-8";
+
+                    await deviceClient.SendEventAsync(message);
+
+                    Console.WriteLine("Message envoyé");
+                    Console.WriteLine(jsonData);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+
+            }
         }
     }
 }
